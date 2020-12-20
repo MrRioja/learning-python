3b5f43808265038d652d6ea59f490038b08f16d8
Rioja <lulyrioja@gmail.com>
2020-12-19 21:12:37 -0300

    :alien: Variables

---
+++ Variables/variables.py
@@ -0,0 +1,7 @@
+name = input("Type your name: ")
+age = int(input("Type your age: "))
+profession = input("Type your profession: ")
+salary = float(input("Type your salary: "))
+
+print("\n" + "The " + profession + " " +
+      name + " has a salary of " + str(salary))
